08aca31c9e9d46833922ee6ac8ff55994021c05a
cluetrain <greenstrat67@gmail.com>
2014-05-30 10:29:35 -0700

    ITC 4/24 view/list and ec1 done

--- application/controllers/mailing_list.php
+++ application/controllers/mailing_list.php
@@ -3,14 +3,38 @@
 
 class Mailing_list extends CI_Controller
 {
+	function __construct(){
+		parent::__construct();
+		$this->load->helper('url');
+	}
+	
 	public function index()
 	{//here we're making data available to our header and footer
 		$this->load->model('Mailing_list_model');
 		$data['query'] = $this->Mailing_list_model->get_mailing_list();
 		
 		$data['title'] = "Here is our title tag!";
+		$data['style'] = "amelia.css";
+		$data['banner'] = "Here is our website!";
+		$data['copyright'] = "Copyright goes here!";
+		$data['base_url'] = base_url();
 		$this->load->view('header',$data);
 		$this->load->view('mailing_list/view_mailing_list',$data);
+		$this->load->view('footer',$data);
+	}
+	
+	public function view($id)
+	{//this will show us the data from a single page
+		$this->load->model('Mailing_list_model');
+		$data['query'] = $this->Mailing_list_model->get_id($id);
+		
+		$data['title'] = "Here is our title tag!";
+		$data['style'] = "amelia.css";
+		$data['banner'] = "Here is our website!";
+		$data['copyright'] = "Copyright goes here!";
+		$data['base_url'] = base_url();
+		$this->load->view('header',$data);
+		$this->load->view('mailing_list/view_mailing_list_detail',$data);
 		$this->load->view('footer',$data);
 	}
 }
--- application/models/mailing_list_model.php
+++ application/models/mailing_list_model.php
@@ -14,5 +14,13 @@ class Mailing_list_model extends CI_Model
 		//return $query->result_array();
 		return $this->db->get('mailing_list');
 	}
+	
+	public function get_id($id)
+	{//will show all data in table named mail_list
+		//$this->db->select('userid,first_name,last_name');
+		$this->db->where('userid',$id);
+		return $this->db->get('mailing_list');
+	}
+	
 }
 ?>
